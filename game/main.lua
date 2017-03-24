dd357d15a135ab48cac7c5790b08e24ea75a785c
DxRedJava <ben.dumouchel@gmail.com>
2017-03-24 18:43:32 -0400

    Basic API stuff

--- game/main.lua
+++ game/main.lua
@@ -1,5 +1,9 @@
 function love.load()
+	love.window.setTitle("Leen Mean Meme Team")
 	lb = require "lib.lovebird"
+	physics = love.physics
+	graphics = love.graphics
+	keyboard = love.keyboard
 end
 
 function love.update(dt)

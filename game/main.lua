6c3a632060ec5c4d71b87ea3bdf5e497dacc950f
DxRedJava <ben.dumouchel@gmail.com>
2017-03-24 19:54:08 -0400

    Added config and gui libraries

--- game/main.lua
+++ game/main.lua
@@ -1,7 +1,9 @@
 function love.load()
 	love.window.setTitle("Leen Mean Meme Team")
-	lb = require "lib.lovebird"
-	physics = love.physics
+	lb       = require "lib.lovebird"
+	gui      = require "lib.gui"
+	config   = require "lib.config"
+	physics  = love.physics
 	graphics = love.graphics
 	keyboard = love.keyboard
 end

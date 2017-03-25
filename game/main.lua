2b1382e6a90041c1a9394261e30ec6dd8065e016
DxRedJava <ben.dumouchel@gmail.com>
2017-03-24 22:25:43 -0400

    Made somethin' Spoopy

--- game/main.lua
+++ game/main.lua
@@ -3,9 +3,15 @@ function love.load()
 	lb       = require "lib.lovebird"
 	gui      = require "lib.gui"
 	config   = require "lib.config"
-	physics  = love.physics
+	physics  = require "lib.physics"
 	graphics = love.graphics
 	keyboard = love.keyboard
+	locale = config.load("locale")
+
+	wWidth,wHeight = love.window.getMode()
+
+	players = {physics.kinetic(0,0,16,16,nil,"player1")}
+	terrain = {physics.static(0,wHeight-20,wWidth,20,nil)}
 end
 
 function love.update(dt)
@@ -13,4 +19,6 @@ function love.update(dt)
 end
 
 function love.draw()
+	physics.draw(players[1])
+	physics.draw(terrain[1])
 end
